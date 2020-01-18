0ec5f6939e16d91cdd8e878ddc87b5b2f339805c
Christos Ziakas <ge73hor@tum.de>
2020-01-18 18:01:41 +0100

    Bernoulli sig

--- embedding/bernoulli.py
+++ embedding/bernoulli.py
@@ -40,39 +40,12 @@ class Bernoulli(StaticGraphEmbedding):
         self._similarity_measure = "adjacency"
         self._W_enabled = W_enabled
 
-    def setup_model_input(self, AdjMat):
+    def setup_model_input(self, adj_mat):
         # input
-        self._num_nodes = AdjMat.shape[0]
-        self._num_edges = AdjMat.sum()
-
-        # Model parameters
-        self._emb = nn.Parameter(torch.empty(self._num_nodes, self._embedding_dim).normal_(0.0, 0.1))
-        self._W = nn.Parameter(torch.empty(self._embedding_dim, self._embedding_dim).normal_(0.0, 0.1))
-        self._edge_proba = self._num_edges / (self._num_nodes ** 2 - self._num_nodes)
-        self._bias_init = np.log(self._edge_proba / (1 - self._edge_proba))
-        self._b = nn.Parameter(torch.Tensor([self._bias_init]))
-        
-
-        self._e1, self._e2 = AdjMat.nonzero()
-        self._e1 = torch.LongTensor(self._e1)
-        self._e2 = torch.LongTensor(self._e2)
-        # self._adj = graph_util.csr_matrix_to_torch_tensor(AdjMat)
-
-        # if(self._decoder == 'sigmoid'):
-        #     self._pos_term, self._neg_term, self._size, _ = sigmoid(self._emb,self._adj)
-        # elif (self._decoder == 'gaussian'):
-        #     self._pos_term, self._neg_term, self._size, _ = gaussian(self._emb,self._adj)
-        # elif (self._decoder == 'exponential'):
-        #     self._pos_term, self._neg_term, self._size, _ = exponential(self._emb,self._adj)
-
-
-        ### Optimizer definition ###
-        # Regularize the embeddings but don't regularize the bias
-        # The value of weight_decay has a significant effect on the performance of the model (don't set too high!)
-        self._opt = torch.optim.Adam([
-            {'params': [self._emb], 'weight_decay': self._weight_decay},
-            {'params': [self._b]}],
-            lr=self._learning_rate)
+       
+        self._num_nodes = adj_mat.shape[0]
+        self._num_edges = adj_mat.sum()
+        self._Adj = adj_mat
 
         self._setup_done = True
 
@@ -112,40 +85,45 @@ class Bernoulli(StaticGraphEmbedding):
         else:
             self._epoch_begin = self._epoch_end
         self._epoch_end += num_epoch
-
-
-        def compute_loss_sig(emb,W, b=0.1, eps=1e-5):
-            if(self._W_enabled):
-                dist = torch.matmul(torch.matmul(emb,W),emb.T)+b
-            else:
-                dist = torch.matmul(emb,emb.T)+b
-            sigdist = 1/(1+torch.exp(dist+eps)+eps)
-            logsigdist = torch.log(sigdist+eps)
-            pos_term = logsigdist[self._e1,self._e2]
-            neg_term = torch.log(1-sigdist+eps)
-            neg_term[np.diag_indices(emb.shape[0])] = 0.0
-            return -(pos_term.sum() + neg_term.sum()) / emb.shape[0]**2
-
-
-        def compute_loss_gaussian(emb, eps=1e-5):
-            gamma = 0.1
-            pdist = ((emb[:, None] - emb[None, :]).pow(2.0).sum(-1) + eps).sqrt()
-            neg_term = torch.log(-torch.expm1(-pdist*gamma) + eps)
-            neg_term[np.diag_indices(emb.shape[0])] = 0.0
-            pos_term = -pdist[self._e1, self._e2]
-            neg_term[self._e1, self._e2] = 0.0
-            return -(pos_term.sum() + neg_term.sum()) / emb.shape[0]**2
         
+        A = self._Adj
+        num_nodes = A.shape[0]
+        num_edges = A.sum()
+        
+        embedding_dim = 64
+        emb = nn.Parameter(torch.empty(num_nodes, embedding_dim).normal_(0.0, 1.0))
 
-        def compute_loss_exponential(emb, eps=1e-5):
-            emb_abs = torch.FloatTensor.abs(emb)
-            dist = -torch.matmul(emb_abs,emb_abs.T)
-            neg_term=dist
-            neg_term[np.diag_indices(emb.shape[0])]=0.0
-            expdist=torch.exp(dist)
-            logdist=torch.log(1-expdist+eps)
-            pos_term = logdist[self._e1,self._e2]
+        edge_proba = num_edges / (num_nodes**2 - num_nodes)
+        bias_init = np.log(edge_proba / (1 - edge_proba))
+        b = nn.Parameter(torch.Tensor([bias_init]))
+        
+        opt = torch.optim.Adam([
+            {'params': [emb], 'weight_decay': 1e-7},
+            {'params': [b]}],
+        lr=1e-2)
+
+        
+      
+        def compute_loss_sig(adj, emb, b=0.0): 
+            logits = emb @ emb.t() + b.cuda()
+            loss = F.binary_cross_entropy_with_logits(logits, adj, reduction='none')
+            loss[np.diag_indices(adj.shape[0])] = 0.0
+            return loss.mean()
+        
+        
+        def compute_loss_gaussian(adj, emb, b=0.0):
+            eps=1e-5
+            N = adj.shape[0]
+            d=64
+            e1, e2 = adj.nonzero()
+            pdist = ((emb[:, None] - emb[None, :]).pow(2.0).sum(-1) + eps).sqrt()
+            neg_term = torch.log(-torch.expm1(-pdist) + 1e-5)
+            neg_term[np.diag_indices(N)] = 0.0
+            pos_term = -pdist[e1, e2]
+            neg_term[e1, e2] = 0.0
             return -(pos_term.sum() + neg_term.sum()) / emb.shape[0]**2
+
+
 
 
         if(self._decoder == "sigmoid"):
@@ -155,20 +133,22 @@ class Bernoulli(StaticGraphEmbedding):
         if(self._decoder == "exponential"):
             compute_loss = compute_loss_exponential
         
-        #### Learning ####
-        # Training loop
-        for epoch in range(self._epoch_begin, self._epoch_end+1):
-            self._opt.zero_grad()
-            loss = compute_loss(self._emb, self._W)
+        adj = torch.FloatTensor(A.toarray()).cuda()
+        
+        for epoch in range(self._epoch_begin, self._epoch_end):
+            opt.zero_grad()
+            loss = compute_loss(adj,emb.cuda(), b)
             loss.backward()
-            self._opt.step()
-            # Training loss is printed every display_step epochs
+            opt.step()
+    # Training loss is printed every display_step epochs
             if epoch % self._display_step == 0 and self._summary_path:
                 print(f'Epoch {epoch:4d}, loss = {loss.item():.5f}')
                 self._writer.add_scalar('Loss/train', loss.item(), epoch)
+        
 
+   
         # Put the embedding back on the CPU
-        emb_np = self._emb.cpu().detach().numpy()
+        emb_np = emb.cpu().detach().numpy()
 
         # Save the embedding
         #         np.savetxt('embedding_' + self._savefilesuffix + '.txt', emb_np)
